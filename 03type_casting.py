fce5c40296c8f58808cc8c5ded34a1168804cf69
worladev <confidenceworlasi@gmail.com>
2023-05-31 04:13:29 +0100

    lesson on type casting

---
+++ 03type_casting.py
@@ -0,0 +1,5 @@
+### The process of converting one data type to another.
+# Two types of conversion, implicit and explicit conversion
+# implicit is performed automatically by python's compiler to prevent data loss
+# explicit is performed by developers by using python functions (str(), int(), float(), ord(), 
+    # hex(), oct(), tuple(), set(), list(), dict(), ...)
