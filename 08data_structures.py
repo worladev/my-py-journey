3e488f1901a975219f60774201d95aca56aa1e12
worladev <confidenceworlasi@gmail.com>
2023-06-08 00:24:36 +0100

    short notes on data structures

---
+++ 08data_structures.py
@@ -0,0 +1,15 @@
+
+### DATA STRUCTURES IN PYTHON
+    ## BUILT-IN
+        # List
+        # Tuple
+        # Dictionary
+        # Set
+    
+    ## USER-DEFINED
+        # Stack
+        # Queue
+        # Tree
+        # Linked List
+        # Graph
+        # HashMap
