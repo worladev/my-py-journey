eee99e7a8ea4e68d0a092e86d15ae2908c6f16f6
worladev <confidenceworlasi@gmail.com>
2023-06-18 22:13:49 +0100

    more lesson on python tuples

--- 10tuples.py
+++ 10tuples.py
@@ -1,17 +1,20 @@
+### CHECK AGAIN
+
+# Two ways to declare a tuple
+# () - parenthesis
+# tuple() - in-built function
 
 # tuples are immutable
 # they can store values of any type.
-t = ('a', 'b', 'a', 'c', 'd', 'e')
-t = tuple()
-print(t.count('a'))
-print(t.index('c'))
-
-for value in t:
-    print(value)
 
 
-name = tuple('eric',)
+# for value in t:
+#     print(value)
+
+name = ('eric',)
+name1 = tuple('eric')
 print(name)
+print(name1)
 
 z = ('eric',)
 
@@ -28,4 +31,22 @@ b = temp
     # tuple assignment
 a, b = b, a
 
-#More soon
+# Tuple supports slicing
+
+# Unpacking
+points = (5, 7, 10)
+x, y, z = points
+k, j, *other = points
+
+print(x, y, z)
+print(other)
+
+# Tuple methods count(), index()
+names = ("Andy", "Enoch", "Sam", "Worlasi", "Andy", "Sam")
+print(names.count('Andy'))
+print(names.index("Sam"))
+
+t1 = "a", "b", "c", "d", "a"  # - without the bracket
+t = ("a", "b", "c", "d", "e")
+print(t.count('a'))
+print(t.index("a"))
