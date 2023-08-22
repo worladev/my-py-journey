ddf0d1bb8a86e935eabde118341d515f934ef4f2
worladev <confidenceworlasi@gmail.com>
2023-08-22 06:17:40 +0000

    more on map() transformation

--- 20extra_functional.py
+++ 20extra_functional.py
@@ -25,12 +25,25 @@ SYNTAX: map(function, iterable[, iterable1, iterable2,..., iterableN])
             -function is any type of python callable (takes and argument and returns a concrete and useful value.)
 '''
 # in-built tranformation function
+
+#Example
+# converting string numbers to integers
 str_nums = ["3", "5", "2", "8", "3", "4"]
-
 int_nums = set(map(int, str_nums))
-
 print(int_nums)
 
+# getting absolute values
+numbers = [-2, -1, 0, 1, 2]
+abs_values = list(map(abs, numbers))
+print(abs_values)
 
+# converting to floating point numbers
+flo_nums = list(map(float, numbers))
+print(flo_nums)
+
+# getting the length of words in a list
+words = ["Come here", "Welcome", "Hello Python"]
+word_len = list(map(len, words))
+print(word_len)
 
 
