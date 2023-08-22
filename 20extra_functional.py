7d10236dba00f73e85ece3d7115e2ead8f789942
worladev <confidenceworlasi@gmail.com>
2023-08-22 07:04:14 +0000

    more on map() transformation

--- 20extra_functional.py
+++ 20extra_functional.py
@@ -24,7 +24,7 @@ a single cumulative value.
 SYNTAX: map(function, iterable[, iterable1, iterable2,..., iterableN])
             ==>function -- any type of python callable that takes an argument and returns a concrete and useful value.
 '''
-# in-built tranformation function
+# ==> in-built tranformation function
 
 #Example
 # converting string numbers to integers
@@ -46,14 +46,28 @@ words = ["Come here", "Welcome", "Hello Python"]
 word_len = list(map(len, words))
 print(word_len)
 
+# ==> If you supply multiple iterables to map(), then the transformation function
+# must take as many arguments as iterables you pass in. the iteration stops at
+# the end of the shortest iterable.  
+#Example
+l1 = [1, 2, 3]
+l2 = [2, 3, 4, 5]
 
-# user defined transformation function
+# pow() takes two arguments x and y and returns x to the power of y.
+power = list(map(pow, l1, l2))
+print(power)
+
+
+# ==> user defined transformation function
 def square(number):
     return number ** 2
 
-numbers = [1, 2, 3, 4, 5]
+numbers2 = [1, 2, 3, 4, 5]
 
-squared = list(map(square, numbers))
+squared = list(map(square, numbers2))
 
 print(squared)
 
+#or using lambda
+squared = list(map(lambda num: num ** 2, numbers2))
+print(squared)
