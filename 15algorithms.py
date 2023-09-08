a4cdcd91c1f10665b8717f5bedfd043bc9c44966
worladev <confidenceworlasi@gmail.com>
2023-09-08 23:54:10 +0000

    modified lesson on some python algorithms

--- 15algorithms.py
+++ 15algorithms.py
@@ -1,14 +1,27 @@
-### Algorithm for a Palindrome
-    # racecar
-str = input('Enter your word: ')
+'''
+BINARY SEARCH
+'''
+def binary_search(list, target):
+    low = 0
+    high = len(list) - 1
 
-def is_palindrome(str):
-    start_index = 0
-    end_index = len(str) - 1
+    while low <= high:
 
-    for x in str:
-        if str[start_index] != str[end_index]:
-            return False
-    return True
+        # mid = (low + high) // 2  # not correct - could cause integer overflow
+        mid = low + (high - low) / 2 # proper way
+        mid_value = list[mid]
 
-print(is_palindrome(str))
+        if mid_value < target:
+            low = mid + 1
+        elif mid_value > target:
+            high = mid - 1
+        else:
+            return mid # value found!
+        
+    return -1 # value not found
+
+
+list = [4, 7, 8, 12, 45, 99, 102]
+
+print(binary_search(list, 8))
+
